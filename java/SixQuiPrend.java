0519de5079a2f8afdc433cec9930d8b04793cba7
Hugo <hugo_mathieu@hotmail.fr>
2020-04-11 00:14:26 +0200

    Implementation de la methode 6QuiPrend

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -5,9 +5,26 @@ public class SixQuiPrend {
     private static final int NB_CARD_MAX = 104;
 
     public static int findNumberOfBullHeads(int value) {
-        // TODO
 
-        return 0;
+        int bullHeads = 1 ;
+
+        if(value % 5 == 0){
+            bullHeads+=1 ;
+        }
+
+        if(value %10 == 0){
+            bullHeads += 1 ;
+        }
+
+        if(value %11 == 0){
+            bullHeads+=4 ;
+        }
+
+        if(value == 55){
+            bullHeads=7 ;
+        }
+        return bullHeads ;
+
     }
 
     public static void gameLoop() {
